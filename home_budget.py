74cc253f99248c99c096035d12b0fde72a3c5174
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-01-04 16:21:21 +0100

    adding_try_except_block

--- home_budget.py
+++ home_budget.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 from budget import Budget 
 
     
@@ -8,25 +8,32 @@ def main():
     budget = Budget(['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage'])
 
     while True:
+        available_action = ["add","remove","q"]
+        
         # Get the user's choice
         action = input('Enter "add" to add a category, "remove" to remove , "q" for exit : ')
+        if action not in available_action:
+            print("This is not allowed action")
         # Add an expense from the budget
         if action == "add":
             budget.budget_category()
-            category = input('Enter the category to add : ').capitalize()
-            amount = int(input('Enter the amount to add : '))
-            budget.add(category, amount)
-            # # Save the updated budget to the CSV file
-            # budget.save('budget.xlsx')
-            
+            try:
+                category = input('Enter the category to add : ').capitalize()
+                amount = int(input('Enter the amount to add : '))
+                budget.add(category, amount)
+            except KeyError as e:
+                # This code will be executed if a KeyError occurs
+                print("Error: key not found in budget list")
         # Remove an expense from the budget
         elif action == "remove" :
             budget.budget_category()
-            category = input('Enter the category to remove: ').capitalize()
-            amount = int(input('Enter the amount to remove: '))
-            budget.remove(category, amount)
-            # # Save the updated budget to the CSV file
-            # budget.save('budget.xlsx')
+            try:
+                category = input('Enter the category to remove : ').capitalize()
+                amount = int(input('Enter the amount to remove : '))
+                budget.remove(category, amount)
+            except KeyError as e:
+                # This code will be executed if a KeyError occurs
+                print("Error: key not found in budget list")
             
         elif action == "q":
             # Save the updated budget to the CSV file
