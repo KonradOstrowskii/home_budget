f5e9e61f32b32e628d70989857e5482272ae207e
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-01-05 12:49:04 +0100

    adding_open_def

--- budget.py
+++ budget.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import os
+
 
 class Budget:
     def __init__(self, categories):
@@ -6,12 +8,16 @@ class Budget:
         self.data = {}
         for category in self.categories:
             self.data[category] = 0
+            
     # Features Add  expenses
     def add(self, category, amount):
         self.data[category] += amount
+        Budget.save(self,'budget.xlsx')
+        
     # Features Remove expenses
     def remove(self, category, amount):
         self.data[category] -= amount
+        Budget.save(self,'budget.xlsx')
     
     def save(self, file_name):
         # Load the existing data from the Excel file
@@ -25,10 +31,37 @@ class Budget:
 
         # Save the combined data to the Excel file
         df.style.set_caption('Budget') \
-            .format({col: '${:,.2f}'.format for col in self.categories}) \
+            .format({col: '${:,.0f}'.format for col in self.categories}) \
             .hide(axis='index') \
             .to_excel(file_name, index=False)
-    # Function showing all budget category  
+                 
+    # Function showing all budget category
+    def open(self,file_name) :
+        
+        path = "/Users/konrad/python_programs/budget.xlsx"
+        isExist = os.path.exists(path)
+        if isExist == True:
+            pass
+        if isExist == False:
+            print("FileNotFoundError")
+            print("Creating New File")
+             # Create a DataFrame with the budget data
+            df = pd.DataFrame([self.data])
+            # Convert the new data to a DataFrame
+            new_df = pd.DataFrame([self.data])
+
+            # Combine the existing data with the new data
+            df = pd.concat([df, new_df], ignore_index=True)
+
+            # Save the DataFrame to the Excel file
+            df.to_excel(file_name, index=False)
+            
+            # Save the combined data to the Excel file
+            df.style.set_caption('Budget') \
+                .format({col: '${:,.2f}'.format for col in self.categories}) \
+                .hide(axis='index') \
+                .to_excel(file_name, index=False)
+            
     def budget_category(self):
         budget_category = ['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage']
         for number , category in enumerate(budget_category):
@@ -40,9 +73,7 @@ class Budget:
     # Function counting chosen one by user
     def count(self):
         fd = pd.read_excel('budget.xlsx')
-        budget_category = ['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage']
-        for number , category in enumerate(budget_category):
-                print(f"{number+1}:{category}")
+        Budget.budget_category(self)
         try:        
             count= input("What would You like to count? : ").capitalize()
             sum =fd[count].sum()
@@ -50,7 +81,4 @@ class Budget:
         except KeyError as e:
                 # This code will be executed if a KeyError occurs
                 print("Error: key not found in budget list")
-        
-     
-
         
