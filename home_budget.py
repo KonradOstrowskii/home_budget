540dbe5ba9ced39bab804564bc9445e93f43377c
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-01-08 14:00:56 +0100

    Update home_budget.py

--- home_budget.py
+++ home_budget.py
@@ -1,4 +1,3 @@
-
 from budget import Budget 
 
     
@@ -7,6 +6,7 @@ def main():
     # Create an instance of the Budget class with the given categories
     budget = Budget(['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage'])
     budget.open("budget.xlsx")
+    category_list = ['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage']
     while True:
         available_action = ["add","remove","q","history","sum"]
         
@@ -20,15 +20,22 @@ def main():
         if action == "add":
             budget.budget_category()
             try:
-                
-                category = input('Enter the category to add : ').capitalize()
-                amount = int(input('Enter the amount to add : '))
-                budget.add(category, amount)
-                
+                for category in category_list :
+                    while True:
+                        # category = input(f'Enter the {category} to add : ').capitalize()
+                        try:
+                            amount = int(input(f'Enter the amount to {category} : '))
+                            budget.add(category, amount)
+                        except ValueError as e:
+                            print("Amount only can be a numbers :")
+                            continue
+                        break
             except KeyError as e:
                 # This code will be executed if a KeyError occurs
                 print("Error: key not found in budget list")
-            
+                
+        elif action == "n":
+            budget.new_row()    
         # Remove an expense from the budget
         elif action == "remove" :
             budget.budget_category()
