6d6cfe6d098cae7dedf6e11d95766995fa389076
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-01-09 21:04:21 +0100

    Update home_budget.py

--- home_budget.py
+++ home_budget.py
@@ -5,10 +5,13 @@ def main():
     
     # Create an instance of the Budget class with the given categories
     budget = Budget(['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage'])
+    # List of all available category
     category_list = ['Rent', 'Utilities', 'Groceries', 'Entertainment', 'Travels', 'Gifts', 'Eating Outside', 'Mortgage']
     while True:
+        # Open budget.xlsx file
         budget.open("budget.xlsx")
         while True:
+            # List of all available action
             available_action = ["add","remove","q","history","sum"]
             
             # Get the user's choice
@@ -16,11 +19,12 @@ def main():
                         
             if action not in available_action:
                 print("This is not allowed action")
+                
             # Add an expense from the budget
-            
             if action == "add":
-                budget.budget_category()
+                # Creating new row for file
                 budget.new_row()
+                budget.budget_category() 
                 try:
                     for category in category_list :
                         while True:
@@ -38,6 +42,7 @@ def main():
                     
             # Remove an expense from the budget
             elif action == "remove" :
+                # Creating new row for file
                 budget.new_row()
                 budget.budget_category()
                 try:
@@ -65,8 +70,7 @@ def main():
                 # Save the updated budget to the Excel file
                 quit()
                 
-        
-
-
+                
+                
 if __name__ == '__main__':
     main()
