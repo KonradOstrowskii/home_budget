be1b81040cef128256c6fe438f738814ebcf352c
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-01-14 09:01:06 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,39 +1,39 @@
-import random
 class User:
+    users = []
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.list_users = []
-        
-    def check_password(self, entered_password):
-        return entered_password == self.password
+    # Function that checking if username already exist
+    @classmethod
+    def check_username_exist(cls, username):
+        for user in cls.users:
+            if user.username == username:
+                return True
+        return False
+    # Function that checking if password contain length of at least 8 char ,is there any digit and at least one char is upper
+    @classmethod
+    def check_valid_password(cls, password):
+        if len(password) < 8:
+            return False
+        if not any(c.isupper() for c in password):
+            return False
+        if not any(c.isdigit() for c in password):
+            return False
+        return True
+# Creating new user   
+def create_new_user():
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
     
-    def create_user(username, password):
-        new_user = User(username, password)
-        print("Yourd username is : ",username,"and password is :",password)
-        print("Creating new user ...")
-        return new_user
+    if User.check_username_exist(username):
+        print("username already exist")
+        return
+    if not User.check_valid_password(password):
+        print("Invalid password, it should be at least 8 characters, contain at least one uppercase letter and one digit")
+        return
+    new_user = User(username, password)
+    User.users.append(new_user)
+    print(f"user {username} created")
     
-    def passwordGenerator():
-        inits = range(32, 127)
-
-        password = ''
-        for i in range(15):
-            password += chr(random.choice(inits))   
-        print(password)
-        return password
-        
-
-    
-    def create_new_user():
-        username = input("Enter a username: ")
-        choice = input("Would You like do generate random password?(Y/N) ")
-        password = ''
-        if choice == "y":
-            User.passwordGenerator() 
-            User.create_user(username, password)
-        elif choice =="n":
-            password = input("Enter a password: ")
-            User.create_user(username, password)
-      
+create_new_user()
 
