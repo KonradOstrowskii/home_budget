fb3e4640104d8a1200764e5e7ae7b6ace3a86314
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-01-21 19:37:32 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,39 +1,79 @@
+import sqlite3
+
+with sqlite3.connect('user_base.db') as db:
+    cursor = db.cursor()
+# Creating DB containing users with ID, username, password
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS User(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+password TEXT NOT NULL);
+""")
+# User class 
 class User:
-    users = []
     def __init__(self, username, password):
-        self.username = username
+        self.username = username 
         self.password = password
-    # Function that checking if username already exist
-    @classmethod
-    def check_username_exist(cls, username):
-        for user in cls.users:
-            if user.username == username:
-                return True
+    # Staticmethod that checking if username already exist in DB   
+    @staticmethod
+    def check_username_exist(username):
+        cursor.execute("SELECT * FROM User WHERE username=?", (username,))
+        if cursor.fetchone():
+            return True
         return False
-    # Function that checking if password contain length of at least 8 char ,is there any digit and at least one char is upper
-    @classmethod
-    def check_valid_password(cls, password):
+    # Staticmethod that checking if password got at least 8 char,at least one upper char and at least one digit
+    @staticmethod
+    def check_valid_password(password):
         if len(password) < 8:
             return False
-        if not any(c.isupper() for c in password):
+        elif not any(c.isupper() for c in password):
             return False
-        if not any(c.isdigit() for c in password):
+        elif not any(c.isdigit() for c in password):
             return False
         return True
-# Creating new user   
+    # Creating new user 
+    def create(self):
+        if User.check_username_exist(self.username):
+            print("username already exist")
+            return
+        if not User.check_valid_password(self.password):
+            print("Invalid password, it should be at least 8 characters, contain at least one uppercase letter and one digit")
+            return
+        cursor.execute("INSERT INTO User (username, password) VALUES (?,?)", (self.username, self.password))
+        db.commit()
+        print(f"user {self.username} created")
+        
+
+
+
 def create_new_user():
-    username = input("Enter a username: ")
-    password = input("Enter a password: ")
-    
-    if User.check_username_exist(username):
-        print("username already exist")
-        return
-    if not User.check_valid_password(password):
-        print("Invalid password, it should be at least 8 characters, contain at least one uppercase letter and one digit")
-        return
-    new_user = User(username, password)
-    User.users.append(new_user)
-    print(f"user {username} created")
-    
+    while True:
+        username = input("Enter a username: ")  
+        password = input("Enter a password: ")
+        if User.check_username_exist(username):
+            print("username already exist")
+            break
+        if not User.check_valid_password(password):
+            print("Invalid password, it should be at least 8 characters, contain at least one uppercase letter and one digit")
+            break
+        new_user = User(username, password)
+        new_user.create()
+        break
+
+
 create_new_user()
 
+# Connect to the database
+conn = sqlite3.connect("user_base.db")
+
+# Create a cursor
+c = conn.cursor()
+
+# Execute a SELECT statement
+c.execute("SELECT * FROM User")
+
+# Retrieve the results
+result = c.fetchall()
+
+# Print the results
+print(result)
