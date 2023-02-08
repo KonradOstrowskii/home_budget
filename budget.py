df7fc83d59c623b34f22c9e6c8673957a0d64274
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-02-08 19:33:06 +0100

    Update budget.py

--- budget.py
+++ budget.py
@@ -4,28 +4,55 @@ import openpyxl
 
 
 class Budget:
-    def __init__(self, categories):
+    def __init__(self, categories: str):
+        """
+        __init__  Creating category list
+
+        Args:
+            categories (str): list of categories
+        """
         self.categories = categories
         self.data = {}
         for category in self.categories:
             self.data[category] = 0
             
-    # Features Add  expenses
-    def add(self, category, amount):
+
+    def add(self, category: list, amount: int):
+        """
+        Features Add  expenses for `category`
+        Save file as `xlsx` file
+
+        Args:
+            category (list): lists of `category`
+            amount (int): amount entered by User
+        """
         self.data[category] = amount
         Budget.save(self,'budget.xlsx')
         
-    # Features Remove expenses
-    def remove(self, category, amount):
+    def remove(self, category : list, amount: int) -> list:
+        """
+        Features remove  expenses for `category`
+        Save file as `xlsx` file
+
+        Args:
+            category (list): _description_
+            amount (int): _description_
+        """
         self.data[category] = -amount
         Budget.save(self,'budget.xlsx')
     
-    def save(self, file_name):
+    def save(self, file_name : object)-> object:
+        """
+        save   1.Load the existing data from the Excel file
+               2.Update the values in the existing row
+               3.Save the changes to the Excel file
+
+        Args:
+            file_name (object): `xlsx` file
+        """
         # Load the existing data from the Excel file
         wb = openpyxl.load_workbook(file_name)
         sheet = wb.active
-
-        # Find the last row of the sheet
         last_row = sheet.max_row
 
         # Update the values in the existing row
@@ -36,7 +63,18 @@ class Budget:
         sheet.insert_rows(sheet.max_row + 1)
         wb.save(file_name)
         
-    def new_row(self,file_name="budget.xlsx"):
+    def new_row(self,file_name: object ="budget.xlsx")-> object:
+        """
+        1.Load the existing data from the Excel file
+        2.Find the last row of the sheet
+        3.Update the values in the existing row
+        
+        Args:
+            file_name (object, optional): Defaults to `"budget.xlsx"`.
+
+        Returns:
+            object: return updated xlsx file
+        """
         # Load the existing data from the Excel file
         wb = openpyxl.load_workbook(file_name)
         sheet = wb.active
@@ -49,7 +87,17 @@ class Budget:
             sheet.cell(row=last_row+1, column=i+1).value = self.data[category]
         wb.save(file_name)
         
-    def open(self,file_name) :
+    def open(self,file_name : object)-> object:
+        """
+        1.Find path for file if exist open it, else creating file indicated path
+        2.Create a DataFrame with the budget data
+        3.Dropping rows with 0 values
+        4.Save the DataFrame to the `Excel` file
+        5.Save the combined data to the `Excel` file
+
+        Returns:
+            object: excel file`xlsx`
+        """
         
         path = "/Users/konrad/python_programs/budget.xlsx"
         isExist = os.path.exists(path)
@@ -76,18 +124,43 @@ class Budget:
             except TypeError as e:
                 print("'builtin_function_or_method' object is not iterable")
                 
-    # Function that enumerate all category list       
-    def budget_category(self,budget_category_show):
+    def budget_category(self,budget_category_show: list)-> list:
+        """
+        Function that enumerate all category list
+
+        Args:
+            budget_category_show (list): `enumerate` category list
+
+        Returns:
+            list: `enumerate` list
+        """
+        # Function that enumerate all category list
         for number , category in enumerate(budget_category_show):
                 return(f"{number+1}:{category}")
                 
-    # Function showing transaction history
-    def transaction_history(self):
+    def transaction_history(self: object)-> object:
+        """
+        Function showing transaction history
+
+        Args:
+            self (object): file `xlsx`
+
+        Returns:
+            object: Return transaction of history
+        """
         fd = pd.read_excel('budget.xlsx')
         print(fd)
         
-    # Function counting all category
-    def count(self,budget_to_count):
+    def count(self,budget_to_count: list) -> list:
+        """
+        Function counting all category
+
+        Args:
+            budget_to_count (list): list of category to count
+
+        Returns:
+            list: Returning list of category and sum of all amounts
+        """
         fd = pd.read_excel('budget.xlsx')
         # Budget.budget_category(self)
         try:
@@ -98,4 +171,8 @@ class Budget:
         except KeyError as e:
                 # This code will be executed if a KeyError occurs
                 print("Error: key not found in budget list")
-               
+                
+   
+            
+
+
