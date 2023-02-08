1a784a736bc00d5478b51c664cc10ac8ab757281
Konrad Ostrowski <97406457+KonradOstrowskii@users.noreply.github.com>
2023-02-08 20:10:55 +0100

    fixing_spelling

--- budget.py
+++ budget.py
@@ -4,12 +4,12 @@ import openpyxl
 
 
 class Budget:
-    def __init__(self, categories: str):
+    def __init__(self, categories: list):
         """
-        __init__  Creating category list
+        __init__  Creating list with categories
 
         Args:
-            categories (str): list of categories
+            categories (list): List of categories
         """
         self.categories = categories
         self.data = {}
@@ -17,35 +17,35 @@ class Budget:
             self.data[category] = 0
             
 
-    def add(self, category: list, amount: int):
+    def add(self, category: str, amount: float):
         """
-        Features Add  expenses for `category`
+        Feature to add expenses for `category`
         Save file as `xlsx` file
 
         Args:
-            category (list): lists of `category`
-            amount (int): amount entered by User
+            category (list): `category` in the `categories` list
+            amount (float): Amount entered by User
         """
         self.data[category] = amount
         Budget.save(self,'budget.xlsx')
         
-    def remove(self, category : list, amount: int) -> list:
+    def remove(self, category : str, amount: float):
         """
-        Features remove  expenses for `category`
+        Feature to remove expenses from `category`
         Save file as `xlsx` file
 
         Args:
-            category (list): _description_
-            amount (int): _description_
+            category (list): `category` in the `categories` list
+            amount (float): Amount entered by User
         """
         self.data[category] = -amount
         Budget.save(self,'budget.xlsx')
     
-    def save(self, file_name : object)-> object:
+    def save(self, file_name : object) -> object:
         """
-        save   1.Load the existing data from the Excel file
-               2.Update the values in the existing row
-               3.Save the changes to the Excel file
+        1.Load the existing data from the Excel file
+        2.Update the values in the existing row
+        3.Save the changes to the Excel file
 
         Args:
             file_name (object): `xlsx` file
@@ -63,17 +63,17 @@ class Budget:
         sheet.insert_rows(sheet.max_row + 1)
         wb.save(file_name)
         
-    def new_row(self,file_name: object ="budget.xlsx")-> object:
+    def new_row(self,file_name: object ="budget.xlsx") -> object:
         """
         1.Load the existing data from the Excel file
         2.Find the last row of the sheet
         3.Update the values in the existing row
         
         Args:
-            file_name (object, optional): Defaults to `"budget.xlsx"`.
+            file_name (object, optional): By default to `"budget.xlsx"`.
 
         Returns:
-            object: return updated xlsx file
+            object: Return updated xlsx file
         """
         # Load the existing data from the Excel file
         wb = openpyxl.load_workbook(file_name)
@@ -87,18 +87,17 @@ class Budget:
             sheet.cell(row=last_row+1, column=i+1).value = self.data[category]
         wb.save(file_name)
         
-    def open(self,file_name : object)-> object:
+    def open(self,file_name: object) -> object:
         """
-        1.Find path for file if exist open it, else creating file indicated path
+        1.Find path to file if exists open it, else create file in indicated path
         2.Create a DataFrame with the budget data
-        3.Dropping rows with 0 values
+        3.Drop rows with 0 values
         4.Save the DataFrame to the `Excel` file
         5.Save the combined data to the `Excel` file
 
         Returns:
             object: excel file`xlsx`
         """
-        
         path = "/Users/konrad/python_programs/budget.xlsx"
         isExist = os.path.exists(path)
         if isExist == True:
@@ -124,17 +123,16 @@ class Budget:
             except TypeError as e:
                 print("'builtin_function_or_method' object is not iterable")
                 
-    def budget_category(self,budget_category_show: list)-> list:
+    def budget_category(self,budget_category_show: list)-> str:
         """
-        Function that enumerate all category list
+        Function to enumerate all categories in the list
 
         Args:
-            budget_category_show (list): `enumerate` category list
+            budget_category_show (list): Enumerated list of categories
 
         Returns:
-            list: `enumerate` list
+            str: Return enumerated elements of the list
         """
-        # Function that enumerate all category list
         for number , category in enumerate(budget_category_show):
                 return(f"{number+1}:{category}")
                 
@@ -153,7 +151,7 @@ class Budget:
         
     def count(self,budget_to_count: list) -> list:
         """
-        Function counting all category
+        Function counting amount from all category
 
         Args:
             budget_to_count (list): list of category to count
@@ -172,7 +170,3 @@ class Budget:
                 # This code will be executed if a KeyError occurs
                 print("Error: key not found in budget list")
                 
-   
-            
-
-
